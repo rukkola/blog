cd676aef6d108e775a9e3346703593f09a779687
serj <seregaproger@yandex.ru>
2020-07-15 23:51:34 +0300

    add Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_134125) do
+ActiveRecord::Schema.define(version: 2020_07_15_180157) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "email"
