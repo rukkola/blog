9b29ae67e33fd86d8161dbecea81eadc27e4e522
serj <seregaproger@yandex.ru>
2020-07-16 00:17:10 +0300

    impliment articles created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
-  end
+
+   @article = Article.new(article_params)
+      if @article.valid?
+        @article.save
+      else
+        render action: 'new'
+      end  
+#      render plain: params[:article].inspect
+    end
+
+ private
+
+ def article_params
+    params.require(:article).permit(:title, :text)  
+ end
 
 end
