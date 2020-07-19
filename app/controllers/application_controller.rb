313e3618ca282f7c88b9637ab24d91e1f7a678b6
serj <seregaproger@yandex.ru>
2020-07-20 01:04:08 +0300

    permit Username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])  # старый вариант  devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+
 end
