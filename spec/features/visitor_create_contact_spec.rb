b16ece1b7c71604146b6641672a47d58a939207f
serj <seregaproger@yandex.ru>
2020-07-21 19:23:20 +0300

    error capybara  =begin  =end

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+=begin
+feature "..." do 
+  scenario " " do
+  end 
+end
+=end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,12 @@
 require "spec_helper"
 
+=begin
 describe Article do
   describe "#subject" do
     it "returns the article title" do
       article = create(:article, title: 'qqq www')
-      eaxept(article.subject).to eq 'qqq www'
+      expect(article.subject).to eq 'qqq www'
     end
   end
 end
+=end
