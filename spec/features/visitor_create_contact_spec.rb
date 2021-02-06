726b17a406abbc0c684b60440c280a31fb12c711
serj <seregaproger@yandex.ru>
2021-02-07 00:19:02 +0300

    update  test "Create contact"

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 feature "Contact creation" do 
   scenario "allows acees to contacts page" do
     visit '/contacts'
-    expect(page).to have_content 'Contact Us!'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end 
 end
