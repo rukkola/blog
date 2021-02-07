796694bf679da6016233e2f0e023ae6cd9924a19
serj <seregaproger@yandex.ru>
2021-02-07 18:33:23 +0300

    add tests: validation, associations, last comment in article_spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,12 +1,27 @@
-require "spec_helper"
+require "rails_helper"
 
-=begin
 describe Article do
+
+  describe "validation" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+  describe "associations" do
+    it { should have_many :comments }
+  end
+
   describe "#subject" do
     it "returns the article title" do
       article = create(:article, title: 'qqq www')
       expect(article.subject).to eq 'qqq www'
     end
   end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      article = create(:article_with_comments)
+      expect(article.last_comment.body).to eq 'Comment body 3'
+    end
+  end
 end
-=end
