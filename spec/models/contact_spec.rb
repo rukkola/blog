fd2b188b7140a7b6eb16038be5a41777248b5bcb
serj <seregaproger@yandex.ru>
2021-02-07 17:20:13 +0300

    add contact_spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,6 @@
-require "spec_helper"
+require "rails_helper"
 
-#describe Contact do
-#  it { should validate_presence_of :email }
-#end
+describe Contact do
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :message }
+end
