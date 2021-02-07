071f6909d334c3ce5932c8b45e56496e5df6c126
serj <seregaproger@yandex.ru>
2021-02-07 18:27:52 +0300

    rename title to text;  add method "last_comment"

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ApplicationRecord
   validates :title, presence: true
-  validates :title, presence: true
+  validates :text, presence: true
   has_many :comments
 
   def subject
     title
+  end
+
+  def last_comment
+    comments.last
   end
 end
